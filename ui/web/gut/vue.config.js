541a001947a66db9fcd228cb8be347f775f8c609
Srinivas <srinivengala@gmail.com>
2018-09-06 22:55:44 -0500

    ui: adding material design and started pwa

--- ui/web/gut/vue.config.js
+++ ui/web/gut/vue.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/ui/'
-    : '/'
+    : '/',
+
+//todo: get pwa working
+/*  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }*/
 }
